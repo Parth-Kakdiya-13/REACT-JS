6a5cc83add0317d1a84701d2b70b219023401092
Parth Patel <110605639+Parth-Kakdiya-13@users.noreply.github.com>
2024-12-28 00:37:20 +0530

    Update index.js

--- MyPortfolio-backend/api/index.js
+++ MyPortfolio-backend/api/index.js
@@ -12,13 +12,13 @@ app.use(cors({
     credentials: true
 }));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Update with your origins if needed
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*'); // Update with your origins if needed
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
 
 
 
