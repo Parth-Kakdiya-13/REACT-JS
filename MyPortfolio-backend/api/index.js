b20944f8bb19059ef251f1c0a846d843049b29e3
parth-kakdiya-13 <parthrkakadiya@gmail.com>
2024-12-28 00:02:34 +0530

    add:portfolio backend

--- MyPortfolio-backend/api/index.js
+++ MyPortfolio-backend/api/index.js
@@ -6,11 +6,31 @@ const app = express();
 
 
 app.use(express.json());
-const allowedOrigins = ['https://portfolio-parths-projects-754f6040.vercel.app/'];
+// const allowedOrigins = [
+//     "https://portfolio-parths-projects-754f6040.vercel.app/"
+// ];
+
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             console.error(`CORS error: Origin ${origin} not allowed`);
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true
+// }));
+const allowedOrigins = ['https://your-frontend-url.com'];
 app.use(cors({
     origin: allowedOrigins,
 }));
 
+
+app.get('/', (req, res) => {
+    res.json("hello")
+})
 
 
 // Route to serve a file
