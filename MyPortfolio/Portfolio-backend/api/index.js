672fb7ce7f4e1d4115f12c66ba221fc0adc1fcb3
parth-kakdiya-13 <parthrkakadiya@gmail.com>
2025-02-13 13:01:41 +0530

    add env on vercel

--- MyPortfolio/Portfolio-backend/api/index.js
+++ MyPortfolio/Portfolio-backend/api/index.js
@@ -94,7 +94,7 @@ app.post('/contact', async (req, res) => {
         })
         res.status(200).json({ message: "data received" })
     } catch (err) {
-        res.status(500).json({ message: err.message })
+        res.status(500).json({ message: err })
     }
 })
 
--- MyPortfolio/Portfolio-frontend/src/API/api.js
+++ MyPortfolio/Portfolio-frontend/src/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://myportfoliobackend-seven.vercel.app"
+    baseURL: "https://myportfoliobackend-eight.vercel.app"
 })
 
 
