6054c1b6c5f662ce9b37d7c280851b610c9d78eb
parth-kakdiya-13 <parthrkakadiya@gmail.com>
2025-03-01 13:34:25 +0530

    update:make post

--- Make Post/post-frontend/src/API/api.js
+++ Make Post/post-frontend/src/API/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://post-backend-eight.vercel.app",
+    baseURL: "http://localhost:3000",
     withCredentials: true
 })
 
