0e8f09c2cc49de4fe73f2c17700bf323ad97ecfa
parth-kakdiya-13 <parthrkakadiya@gmail.com>
2025-03-08 23:40:38 +0530

    changes in post backend

--- MakePost/post-backend/api/app.js
+++ MakePost/post-backend/api/app.js
@@ -14,7 +14,7 @@ const commentRoutes = require('./routes/comment')
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(
     cors({
         origin: ["https://my-post-plum.vercel.app", "http://localhost:5173"], // Fixed Typo
@@ -47,15 +47,6 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24 // 1 day
     }
 }))
-
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     res.header("Access-Control-Allow-Headers", "Content-Type, multipart/form-data");
-//     res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
-//     next();
-// });
 
 app.use(async (req, res, next) => {
     if (!req.session.user || !mongoose.Types.ObjectId.isValid(req.session.user._id)) {
