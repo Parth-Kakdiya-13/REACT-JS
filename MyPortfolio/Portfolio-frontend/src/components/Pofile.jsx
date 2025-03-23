a7ad6470772151ff94a1af48bd324d1b98bfd5d0
parth-kakdiya-13 <parthrkakadiya@gmail.com>
2025-03-23 10:56:52 +0530

    update UI

--- MyPortfolio/Portfolio-frontend/src/components/Pofile.jsx
+++ MyPortfolio/Portfolio-frontend/src/components/Pofile.jsx
@@ -1,5 +1,7 @@
 import profilePicture from '../assets/pk_01.jpg';
 import API from '../API/api';
+import github from '../assets/github.png'
+import linkedin from '../assets/linkedin.png'
 
 const Profile = ({ theme }) => {
     console.log(theme);
@@ -25,11 +27,26 @@ const Profile = ({ theme }) => {
         }
     };
 
+    const linkedInLink = () => {
+        // Redirect to the desired link
+        window.location.href = "https://www.linkedin.com/in/parth-kakdiya";
+    };
+
+    const githubLink = () => {
+        // Redirect to the desired link
+        window.location.href = "https://github.com/parth-Kakdiya-13";
+    };
+
 
     return (
         <main className={`w-5/12 max-md:w-full flex items-center rounded-ms ${theme === 'light' ? 'bg-stone-200' : ' bg-stone-800'} transition-all duration-700 ease-in  z-20 profile`}>
             <div className='flex flex-col items-center'>
-                <img src={profilePicture} className='w-1/2 h-fit' />
+                <img src={profilePicture} className='w-1/2 max-md:w-2/5 h-fit object-cover' />
+                <h1 className='py-5 text-3xl font-thin'>Parth Kakdiya</h1>
+                <div className='flex gap-2'>
+                    <img src={github} className='w-8 h-8 bg-violet-200 p-1 rounded-md cursor-pointer' onClick={githubLink} />
+                    <img src={linkedin} className='w-8 h-8 bg-violet-200 p-1 rounded-md cursor-pointer' onClick={linkedInLink} />
+                </div>
                 <button className='w-fit rounded-xl px-5 py-2 bg-purple-500 text-white mt-5' onClick={() => handleDownload('parth_kakdiya.pdf')}>Download Resume</button>
             </div>
         </main>
